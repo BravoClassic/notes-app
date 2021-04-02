8fd6a6d8ae5d568b1fa400e6d15720ef1ea72019
BravoClassic <gerald.akorli@gmail.com>
2021-04-03 02:40:32 +0530

    add first name and last name fields

--- register.php
+++ register.php
@@ -1,5 +1,7 @@
 <?php include 'header.php';?>
 <form class="form" action="inc/register.php" method="post">
+  <input type="text" name="fname" value="" placeholder="Enter First Name">
+  <input type="text" name="lname" value="" placeholder="Enter Last Name">
   <input type="email" name="email" value="" placeholder="Enter email address">
   <input type="password" name="password" value="" placeholder="Enter password">
   <input type="password" name="password2" value="" placeholder="Enter password again">
