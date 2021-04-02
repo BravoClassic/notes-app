8ddf4545c6f2c765d65f00c1b2897120960191db
BravoClassic <gerald.akorli@gmail.com>
2021-04-03 02:11:29 +0530

    write basic code for structure of app

---
+++ index.php
@@ -0,0 +1,27 @@
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Notes App</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="brand">
+          <a href="#"><img src="#" alt="brand logo"></a>
+        </div>
+        <ul>
+          <a href="#">
+            <li>Home</li>
+          </a>
+          <a href="#">
+            <li>Login</li>
+          </a>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>Online Notes App</h1>
+    </main>
+  </body>
+</html>
