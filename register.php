56b4689b99d291007e076846a1942ae79ceb7437
BravoClassic <gerald.akorli@gmail.com>
2021-04-03 02:21:52 +0530

    Created the html form for registering into notes app

---
+++ register.php
@@ -0,0 +1,7 @@
+<?php include 'header.php';?>
+<form class="form" action="inc/register.php" method="post">
+  <input type="email" name="email" value="" placeholder="Enter email address">
+  <input type="password" name="password" value="" placeholder="Enter password">
+  <input type="password" name="password2" value="" placeholder="Enter password again">
+</form>
+<?php include 'footer.php'; ?>
