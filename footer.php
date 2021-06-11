0dfc3ba2dc48c6c4ace1216f8d99d9384cf11ea2
BravoClassic <gerald.akorli@gmail.com>
2021-06-11 04:11:36 +0000

    added the fontawesome script

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
-<script src="assets/js/index.js"></script>
+<?php
+session_start();
+if(isset($_SESSION['id'])):
+?>
+    <script src="assets/js/app.js"></script>
+<?php else: ?>
+    <script src="assets/js/index.js"></script>
+<?php endif; ?>
+<script src="https://kit.fontawesome.com/2e803aa4a5.js" crossorigin="anonymous"></script>
 </body>
 </html>
