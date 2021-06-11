e00eca86f861396a6d5b96f750a52fba84dc3b53
BravoClassic <gerald.akorli@gmail.com>
2021-06-11 04:10:33 +0000

    removed unneccessary code and included header and footer.php

--- app.php
+++ app.php
@@ -1,128 +1,37 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<?php include 'header.php'; ?>
 
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <title>Notes App</title>
-</head>
-
-<body>
-  <header class="header-app-nav">
-    <nav>
-      <div class="brand">
-        <a href="index.php"><img src="assets/logo.png" alt="brand logo"></a>
-        <span>Menu</span>
-      </div>
-      <ul class="nav-links">
-        <a href="#">
-          <li>All Notes</li>
-        </a>
-        <a href="#">
-          <li>Favourites</li>
-        </a>
-      </ul>
-    </nav>
-  </header>
+  
   <main class="container">
     <div class="create-view-note ">
       <section class="create-edit-note hidden">
-        <form class="create-edit-form" action="#" method="get">
+        <form class="create-edit-form" name="create-edit-form">
+          <input type="text" name="note-id" class="hidden" placeholder="id of Note" disabled>
           <input type="text" name="note-title" id="note-title" placeholder="Title of Note">
           <textarea  name="note-content" placeholder="Enter Note" id="note-content"></textarea>
-          <input type="submit" value="Create Note">
+          <span>
+          <label for="fav">Favorite</label>
+          <input type="checkbox" name="fav" id="fav">
+          </span>
+          <input type="submit" class="create-edit-btn"value="Create Note">
         </form>
       </section>
       <section class="note-list">
         <form class="search-bar" action="#" method="get">
           <input type="search" name="search" id="searchField">
-          <button type="submit">S</button>
+          <button type="submit"><i class="fa fa-search"></i></button>
         </form>
         <ul class="note-list-items">
-          <a href="#">
+          <!-- <a href="#">
             <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit  amet consectetur adipisicing elit amet  elit....</p><!-- 106wchar-->
-            </li>
-          </a>
-          <a href="#">
-            <li>
-              <h1>Title <span></span></h1>
+              <h1>Title <span><i class="fa-regular fa-heart"></i></span><span><i class="fa-solid fa-trash"></i></span></h1>
               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit  amet consectetur adipisicing elit amet  elit....</p>
             </li>
-          </a>
-          <a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a><a href="#">
-            <li>
-              <h1>Title <span></span></h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit....</p>
-            </li>
-          </a>
+          </a> -->
+          <!-- 106wchar-->
+          loading..
         </ul>
       </section>
     </div>
+    <a href="#" class="create-btn"><i class="fa fa-plus"></i></a>
   </main>
+<?php include 'footer.php'; ?>
