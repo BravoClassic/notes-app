f2899e60dbc9e4f60fb0f5d42f0bb318e143f6ab
BravoClassic <gerald.akorli@gmail.com>
2021-06-11 04:19:41 +0000

    🔧rectified some errors

--- inc/login.inc.php
+++ inc/login.inc.php
@@ -4,15 +4,12 @@ include_once 'db.inc.php';
 include 'functions.inc.php';
 
 
-$email = mysqli_real_escape_string($conn, $_POST["email"]);
-$pwd = mysqli_real_escape_string($conn, $_POST["pwd"]);
-
-
+$email = mysqli_real_escape_string($conn, $_POST["emailLogin"]);
+$pwd = mysqli_real_escape_string($conn, $_POST["pwdLogin"]);
 if (!empty($email) || !empty($pwd)) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         if (!empty($pwd)) {
-            login($conn, $email, $password);
-            echo 'success';
+            login($conn, $email, $pwd);
         } else {
             echo "Enter a password!";
         }
